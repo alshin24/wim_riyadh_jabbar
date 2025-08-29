bad7b7ae75c16f7ac86757f0ee5f3983889d23d0
alshin24 <riyadhirham24@gmail.com>
2025-08-29 09:47:59 +0700

    Add files via upload

---
+++ sepeda.php
@@ -0,0 +1,32 @@
+<?php
+session_start();
+$_SESSION['menu'] = "sepeda";
+include "includes/best_url.php";
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.13.1/font/bootstrap-icons.min.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
+    <title>Wim Riyadh</title>
+
+</head>
+
+<body>
+    <div class="container-full">
+
+        <?php
+
+        include "includes/header.php";
+        include "components/sepeda/konten.php";
+        include "includes/footer.php";
+        ?>
+    </div>
+</body>
+
+</html>
